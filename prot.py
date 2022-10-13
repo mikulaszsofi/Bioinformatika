0dfdb6601189222c46e952918a33081f6d56c2f5
mikulaszsofi <zsofia@mikulas.net>
2022-10-13 23:05:11 +0200

    hf

--- prot.py
+++ prot.py
@@ -1,16 +1,27 @@
-def prot():
+def lista():
     f = open("../Bioinformatika/prot", "r").read()
     s = [f[i:i+3] for i in range(0, len(f), 3)] # hármasával szétvágás
-    print(s)
+    return s
 
 def dict():
     f = open("codontable", "r").read()
     f = f.replace("\n"," ")
+
     f = list(f.split(" "))
-    f.remove('') #ez csak az elsőt távolítja el
+    c = f.count('')
+    while c>0:
+        f.remove('')
+        c=c-1
+    #f.remove('') #ez csak az elsőt távolítja el
 
-    print(f)
+    return f
 
-#prot()
+def prot(l,m):
+    dna=''
+    for e in l:
+        x = m.index(e)
+        if m[x+1] !='Stop':
+            dna=dna+m[x+1]
+    print(dna)
 
-dict()
+prot(lista(),dict())
