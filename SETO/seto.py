8813e8dcaa1f44226256feb510b18a476d72ac52
mikulaszsofi <zsofia@mikulas.net>
2022-11-18 13:44:45 +0100

    hf

---
+++ SETO/seto.py
@@ -0,0 +1,17 @@
+def seto(n,A,B):
+    C = set(range(1,n+1))
+    u = A.union(B)
+    i = A.intersection(B)
+    ad = A.difference(B)
+    bd = B.difference(A)
+    ca = C.difference(A)
+    cb = C.difference(B)
+
+    print(u)
+    print(i)
+    print(ad)
+    print(bd)
+    print(ca)
+    print(cb)
+
+f = open("seto.rosalind","r").read().split("\n")
---
+++ SSET/sset.py
@@ -0,0 +1,7 @@
+def sset(n):
+    x = 1
+    for i in range(n):
+        x=(x*2)%1000000
+    return x
+
+print(sset(937))
